ce7cf410cdddee08ce0055842d81dd3191e6defa
RedWolf-py <ale.a.s610@gmail.com>
2022-11-15 22:09:56 -0300

    atualizacao

---
+++ json.js
@@ -0,0 +1,133 @@
+let ferragens = [
+    {
+        img: "/ferragens/portaP(1).png",
+        nome: "Porta Pivoltante"
+    },
+    {
+        img: "/ferragens/portaP(2).png",
+        nome: "Tipo das Ferragens"
+    }, {
+        img: "/ferragens/correrVA(1).png",
+        nome: "Porta 2 folhas"
+    }, {
+        img: "/ferragens/correrVA(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/portaVV(1).png",
+        nome: "Porta 4 Folhas"
+    },
+    {
+        img: "/ferragens/portaVV(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/jumbo(1).png",
+        nome: "Porta Pivoltante Jumbo"
+    },
+
+    {
+        img: "/ferragens/jumbo(3).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/jumbo(4).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/jumbo(5).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/fechoFA(1).png",
+        nome: "Janela 2 Folhas"
+    },
+    {
+        img: "/ferragens/fechoFA(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/fechoVV(1).png",
+        nome: "Janela 4 Folhas"
+    },
+    {
+        img: "/ferragens/fechoVV(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/bascula(1).png",
+        nome: "Bascula"
+    },
+    {
+        img: "/ferragens/bascula(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/Jpivoltante(1).png",
+        nome: "Janela Pivoltante"
+    },
+    {
+        img: "/ferragens/Jpivoltante(1).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/ferragensfixo(1).png",
+        nome: "Ferragens Fixo Meio"
+    },
+    {
+        img: "/ferragens/ferragensfixo(2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/ferragensfixo(3).png",
+        nome: "Ferragens Fixo Canto"
+    },
+    {
+        img: "/ferragens/ferragensfixo(4).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/puchador (1).png",
+        nome: "Puxador"
+    },
+    {
+        img: "/ferragens/puchador (2).png",
+        nome: "Tipo das Ferragens"
+    },
+    {
+        img: "/ferragens/maoamigaVR.jpg",
+        nome: "Mão Amiga"
+    },
+    {
+        img: "/ferragens/maoamigaV.jpg",
+        nome: "Especificação Mão amiga"
+    },
+    {
+        img: "/ferragens/especificacaomaoamigaV.jpg",
+        nome: "Modelo Mão amiga instalada"
+    },
+    {
+        img: "/ferragens/roldanaN.jpg",
+        nome: "Roldana Modelo 1"
+    },
+    {
+        img: "/ferragens/roldanaN2.jpg",
+        nome: "Roldana Modelo 2"
+    },
+    {
+        img: "/ferragens/roldanaN3.jpg",
+        nome: "Roldana Modelo"
+    },
+    {
+        img: "/ferragens/especificacaoroldanaN.jpg",
+        nome: "Especificação Roldana"
+    },
+    {
+        img: "/ferragens/maoamigaN.jpg",
+        nome: "Mão amiga Deslisante"
+    },
+    {
+        img: "/ferragens/gabaritomaoamigaN.jpg",
+        nome: "Especificação Mão Amiga"
+    }
+]
--- main.js
+++ main.js
@@ -8,11 +8,79 @@ var xys = (e) => {
   return document.querySelectorAll(e);
 }
 
+//Variaveis de img
+
+let pi = "/imgV/pivoltante.jpg"
+let ba = "/imgV/bascula.jpg"
+let ja2 = "/imgV/janela2.jpg"
+let ja4 = "/imgV/janela4.jpg"
+let po2 = "/imgV/porta2.jpg"
+let po4 = "/imgV/porta4.jpg"
+let pa = "/imgV/pabrir.jpg"
+let pa2 = "/imgV/pabrir2.jpg"
+let co = "/imgV/correratras.jpg"
+let co2 = "/imgV/correatras2B.jpg"
+let pj = "/imgV/portajumbo.jpg"
+let vs = "/imgV/Versatik.jpg"
+let dl = "/imgV/deslisante.jpg"
+let lg = "/imgV/vidros.png"
+
+//variaveis do projetos
+
+let piX = document.getElementById('piX')
+let baX = document.getElementById('baX')
+let coX = document.getElementById('coX')
+let coX2 = document.getElementById('coX2')
+let jaX2 = document.getElementById('jaX2')
+let jaX4 = document.getElementById('jaX4')
+let maX = document.getElementById('max')
+let poX2 = document.getElementById('poX2')
+let poX4 = document.getElementById('poX4')
+let paX = document.getElementById('paX')
+let paX2 = document.getElementById('paX2')
+let veX = document.getElementById('veX')
+let deX = document.getElementById('deX')
+
+const arr = [pi, ba, ja2, ja4, po2, po4, pa, pa2, co,co2, pj, vs, dl];
+
+// variaveis de calculos
+
+let calcularSoma;
+const somararr = [];
+const arr64 = [];
+const imgconvert = [];
+const localarray = [];
+
+// funcao de animacao do background
+function Animation() {
+  xy(".img-bg img").animate([
+
+    { transform: 'scale(2.0)' },
+    { transform: 'scale(1.0)' }
+  ], {
+
+    duration: 15000,
+    iterations: 1
+  });
+}
+
+Animation();
+
+setInterval(Animation, 600000)
+
+setInterval(() => {
+  localStorage.clear();
+}, 1200000)
+
+// Menu hamburguer para mobile
 
 var menu = xy('.menu');
+var calcular = xy('.calcular');
+const SalvarPdf = xy('.salvarorcamento');
+const ViaCliente = xy('.ViaCliente')
 
 menu.addEventListener('click', function () {
-  var diferente = xy('.mover').classList.toggle("mostrar");
+  xy('.mover').classList.toggle("mostrar");
 });
 
 var tagLi = document.getElementsByTagName('li');
@@ -25,224 +93,142 @@ for (let i = 0; i < tagLi.length; i++) {
   });
 }
 
-var iconBx = xys('.iconBx');
-
+//evento de click nos botoes de navegação mobile
+var btli = xys('.btli');
 var contentBx = xys('.contentBx');
+var btn3 = xy('.btI');
 
-
-for (var i = 0; i < iconBx.length; i++) {
-  var clicou = iconBx[i].addEventListener("click", function () {
+for (var i = 0; i < btli.length; i++) {
+  var clicou = btli[i].addEventListener("click", function () {
     for (var i = 0; i < contentBx.length; i++) {
       contentBx[i].className = 'contentBx';
-
     }
     document.getElementById(this.dataset.id).className = 'contentBx active';
-
   })
-
 }
 
-var calcular = xy('.calcular');
-
-var somaFinal = [];
-var arrayconta = [];
-
-var somar2;
-var calcularSoma;
+// Funçõa para calcular valor metro quadrado do vidro
 
 function metroQuadrado() {
 
-  var clienteP = xy('.cliente').value;
-  var vidroE = xy('.espessura').value;
-  var vidroM = xy('.modelo2').value;
   var altura = xy('.altura').value;
   var largura = xy('.largura').value;
-  var ferragen = xy('.ferragen').value;
-  var material = xy('.material').value;
   var preco = xy('.preco').value;
-
   var resultado = xy('.resultado');
-
   var alturaStr = altura.replace(/\D+/g, '');
   var larguraStr = largura.replace(/\D+/g, '');
   var precoStr = preco.replace(/\D+/g, '');
-  var ferragenStr = ferragen.replace('', '0');
-  var materialStr = material.replace('', '0');
+
   var valorInt = parseInt(alturaStr) * parseInt(larguraStr);
-
   var valorFormatado = (valorInt / (100));
-
   var calcularVidroPreco = (valorFormatado * (precoStr) / 100);
-
   var vidroPreco = calcularVidroPreco.toFixed();
+  resultado.innerHTML = `R$: ${vidroPreco}`;
+  somararr.push(vidroPreco)
+  somarArray();
+  SalvarOrcamento()
+  xy('.altura').value = "";
+  xy('.largura').value = "";
 
-
-  var conversao = [parseInt(vidroPreco), parseInt(ferragenStr), parseInt(materialStr)];
-
-  var somar = conversao.reduce(function (somar, i) {
-    return somar + i;
-  });
-
-
-
-  resultado.innerHTML = `R$: ${somar}`;
-
-  somar2 = somar
-
-
-  arrayconta.push(somar2)
-  campoModel();
-  somarArray()
 };
 
-calcular.addEventListener('click', metroQuadrado);
+calcular.addEventListener('click', campoModel);
 
+// limpar os campos do imput
 
-function limpar() {
+ViaCliente.addEventListener('click', () => {
+  AddImage();
 
-  var clienteL = xy('.cliente').value = "";
-  var altura = xy('.altura').value = "";
-  var largura = xy('.largura').value = "";
-  var ferragen = xy('.ferragen').value = "";
-  var material = xy('.material').value = "";
-  //var vidroE = xy('.espessura').value = "";
-  //var vidroM = xy('.modelo2').value = "";
+  xy('.cliente').value = "";
+  xy('.preco').value = "";
+  xy('.altura').value = "";
+  xy('.largura').value = "";
+  xy('.clienteFinal').value = "";
+ 
+  DimencaoImg();
+});
+
+//funcao somar valores dos array
+
+function somarArray() {
+
+  let rt7 = xy('.result7')
+  calcularSoma = somararr.reduce(function (calcularSoma, index) {
+    return parseInt(calcularSoma) + parseInt(index);
+  });
+  rt7.innerHTML = `${calcularSoma}`
+
 }
+// funnção responssavel de selecionar os modelos dos projetos
 
 function campoModel() {
 
-  //let clienteX = xy('.cliente').value;
-  let espessuraX = xy('.espessura')
   let modeloX = xy('.modelo2')
-
+  let espessuraX = xy('.espessura')
+  let clienteX = xy('.cliente')
   let sessao = modeloX.options[modeloX.selectedIndex];
   let sessaoFinal = sessao.value;
-
+  let sessaocliente = clienteX.value;
   let espessuraZ = espessuraX.options[espessuraX.selectedIndex];
   let espessuraFinal = espessuraZ.value;
 
-  if (sessaoFinal.length <= 0) { 
-    alert('Nome Do Cliente e Modelo é obrigatorio ser Preenchido')
+  if (sessaoFinal.length <= 0 || espessuraFinal.length <= 0 || sessaocliente.length <= 0) {
+    alert('Nome Do Cliente, Espessura e Modelo Do Vidro são obrigatorio ser Preenchido')
 
+  } else {
+    metroQuadrado()
   }
 
 }
 
-var orcamentoFinal = [];
+// Salvando tudo no localStorage
 
-const salvarorcamento = xy('.salvarorcamento');
-
-salvarorcamento.addEventListener("click", () => {
+function SalvarOrcamento() {
 
   var objeto = new Array();
-
   if (JSON.parse(localStorage.getItem("objeto")) != null) {
-    orcamentoFinal.push(JSON.parse(localStorage.getItem("objeto")));
+    localarray.push(JSON.parse(localStorage.getItem("objeto")));
   }
-  var clienteP = xy('.cliente').value;
   var vidroE = xy('.espessura').value;
   var vidroM = xy('.modelo2').value;
   var alturaV = xy('.altura').value;
   var larguraV = xy('.largura').value;
-  var ferragenV = xy('.ferragen').value;
-  var materialV = xy('.material').value;
-  var result7 = xy('.result7')
-  //var valorbrutoV = xy('.resultado').value;
-  //var valorclienteV = xy('.resultadofinal').value;
+  var result10 = xy('.result10')
+  var c2 = xy('.contente21')
 
-  var tipoVidro = [clienteP, vidroM, vidroE, alturaV, larguraV, ferragenV, materialV, somar2];
-
-  orcamentoFinal.push(tipoVidro);
-
-  var pessoaJson = JSON.stringify(orcamentoFinal);
+  var tipoVidro = [vidroM, vidroE, alturaV, larguraV, calcularSoma];
+  localarray.push(tipoVidro);
+  var pessoaJson = JSON.stringify(localarray);
   localStorage.setItem("objeto", pessoaJson);
+  c2.insertAdjacentHTML('beforeend', `<p id="modeloespessura">${['Modelo:' + ' ' + vidroM + ' ' + ' ' + 'Espessura:' + ' ' + vidroE]}</p>`);
 
+  c2.insertAdjacentHTML('beforeend', `<p id="alturaLargura">${['Altura:' + ' ' + alturaV + ' ' + "X" + ' ' + larguraV + ' ' + 'Lagura']}</p>`);
 
+  result10.insertAdjacentHTML('beforeend', `<p id="modeloespessura2">${['Modelo:' + ' ' + vidroM + ' ' + ' ' + 'Espessura:' + ' ' + vidroE]}</p>`);
+  result10.insertAdjacentHTML('beforeend', `<p id="alturaLargura2">${['Altura:' + ' ' + alturaV + ' ' + "X" + ' ' + larguraV + ' ' + 'Lagura']}</p>`);
+  result10.insertAdjacentHTML('beforeend', `<p id="somaBruta">${[`Valor Bruto R\$ ${calcularSoma}`]}</p>`);
+};
 
-  result2.insertAdjacentHTML('beforeend', `<p id="clienteP">${clienteP}</p>`);
-
-  result2.insertAdjacentHTML('beforeend', `<p id="modeloespessura">${['Modelo:' + ' ' + vidroM + ' ' + ' ' + 'Espessura:' + ' ' + vidroE]}</p>`);
-
-  result2.insertAdjacentHTML('beforeend', `<p id="alturaLargura">${['Altura:' + ' ' + alturaV + ' ' + "X" + ' ' + larguraV + ' ' + 'Lagura']}</p>`);
-
-
- // result2.insertAdjacentHTML('beforeend', `<p id="materialA">${[`Material em Aluminio R\$${materialV}`]}</p>`);
-
-  //result2.insertAdjacentHTML('beforeend', `<p id="materialF">${[`Material De Ferragens R\$ ${ferragenV}`]}</p>`);
-
-  result2.insertAdjacentHTML('beforeend', `<p id="somaBruta">${[`Valor Bruto R\$ ${somar2}`]}</p>`);
-
-  result3.insertAdjacentHTML('beforeend', `<p id="modeloespessura">${['Modelo:' + ' ' + vidroM + ' ' + ' ' + 'Espessura:' + ' ' + vidroE]}</p>`);
-
-  result3.insertAdjacentHTML('beforeend', `<p id="alturaLargura">${['Altura:' + ' ' + alturaV + ' ' + "X" + ' ' + larguraV + ' ' + 'Lagura']}</p>`);
-
-
-  result4.insertAdjacentHTML('beforeend', `<p id="alturaLarguraPF">${['Altura:' + ' ' + alturaV + ' ' + "X" + ' ' + larguraV + ' ' + 'Lagura' + ' ' + 'R$:' + ' ' + 'Valor:' + ' ' + somar2]}</p>`);
-
-  alert("Salvo com Sucesso !");
-
-  limpar()
-  somarArray()
-
-  result7.innerHTML = `Valor Total: R$: ${calcularSoma}`
-
-});
-
-let data = new Date();
-let dia = String(data.getDate()).padStart(2, '0')
-let mes = String(data.getDate() + 1).padStart(2, '0')
-let ano = data.getFullYear();
-let hoje = dia + '/' + mes + '/' + ano
-
-
-
-
-function somarArray() {
-
-  let inicioA = 0
-  for (let i = 0; i < arrayconta.length; i++) {
-
-    calcularSoma = inicioA += arrayconta[i];
-
-
+// salvar nome do cliente
+function SalvarInput() {
+  let clienteP = xy('.cliente').value;
+  let c1 = xy('.contente21')
+  let objeto = new Array();
+  if (JSON.parse(localStorage.getItem("objeto")) != null) {
+    localarray.push(JSON.parse(localStorage.getItem("objeto")));
   }
+  localarray.push([clienteP]);
+  let pessoaJson = JSON.stringify(localarray);
+  localStorage.setItem("objeto", pessoaJson);
+  c1.insertAdjacentHTML('beforeend', `<p id="clienteP2">${clienteP}</p>`);
+  
 
 }
 
 
-function gerarPdf() {
-
-  let clienteP = xy('#clienteP').innerHTML;
-  let dados = xy('#result3').innerHTML;
 
 
-  var janela = window.open("width=1000,height=800");
-  janela.document.write('<html><head>');
-  janela.document.write(`<title>Data do Orçamento ${hoje}</title></head>`);
-
-  janela.document.write('<body>');
-  janela.document.write('<p>======================================================================================</p>');
-  janela.document.write('<h1>');
-  janela.document.write('<center>Vidraçaria Modesto <img width="60" height="50" src="/imgV/vidros.png"/></center>')
-  janela.document.write('</h1>');
-  janela.document.write('<p>======================================================================================</p>');
-
-  janela.document.write('<p>======================================================================================</p>');
-
-  janela.document.write(`<h1><center>${clienteP}</center></h1>`);
-  ('<p>======================================================================================</p>');
-  janela.document.write(`<h2><p>${dados}</p></h2>`);
-
-  //janela.document.write('<p>'+dados+'</p>')
-
-  janela.document.write(`<p>Data do Orçamento ${hoje}</p>`);
-
-  janela.document.write('</budy></html>');
-  janela.document.close();
-  janela.print();
-
-}
-
+// funcao do menu desktop para alterar a cor do menu clicado
 
 document.querySelectorAll('.cor').forEach(function (hove) {
   hove.addEventListener('click', function (e) {
@@ -252,6 +238,8 @@ document.querySelectorAll('.cor').forEach(function (hove) {
   });
 })
 
+// funcao de adicionar eventos nos campos dos inputs
+
 var footerClick = xys('.preco,.altura,.largura, .tot, .cliente');
 
 for (var i = 0; i < footerClick.length; i++) {
@@ -260,77 +248,130 @@ for (var i = 0; i < footerClick.length; i++) {
 
   })
 };
+
+
+//modelo dos vidros do painel mostrar modelo e salvar o que o cliente quer
+
 function ModeloVi() {
-  let result05 = document.getElementById('result5')
+
+  let imgDi = xy('.imgDi')
   let MdOptions = xy('.modelo2').value;
-  //let MdOptions = somaMd.options[somaMd.selectedIndex].value;
-
-  let portaabrir = xy('.portaabrir');
-  let portacorreatras = xy('.portacorreratraz');
-  let portacorrer4 = xy('.porta4');
-  let janelas = xy('.janelas');
-  let pivoltanteX = xy('.pivoltanteX');
-  let basculaX = xy('.basculaX');
-  let deslisanteporta = xy('.deslisanteporta');
-  let versatikporta = xy('.versatikporta');
-
-  if (MdOptions == 'portaCorrervao') {
-    portacorrer4.style.display = 'block'
-    result05.appendChild(portacorrer4)
-  } else {
-    portacorrer4.style.display = 'none'
-  }
-
-  if (MdOptions == 'portaPivoltante') {
-    portaabrir.style.display = 'block'
-    result05.appendChild(portaabrir)
-  } else {
-    portaabrir.style.display = 'none'
-  }
-
-  if (MdOptions == 'portaatrazparede') {
-    portacorreatras.style.display = 'block'
-    result05.appendChild(portacorreatras)
-  } else {
-    portacorreatras.style.display = 'none'
-  }
-
-  if (MdOptions == 'Janela') {
-    janelas.style.display = 'block'
-    result05.appendChild(janelas)
-  } else {
-    janelas.style.display = 'none'
-  }
 
   if (MdOptions == 'bascula') {
-    basculaX.style.display = 'block'
-    result05.appendChild(basculaX)
-  } else {
-    basculaX.style.display = 'none'
+    imgDi.innerHTML = ""
+    mgb = document.createElement('img')
+    mgb.src = ba
+    imgDi.appendChild(mgb)
+    arr64.push(baX)
   }
 
-  if (MdOptions == 'pivotante') {
-    pivoltanteX.style.display = 'block'
-    result05.appendChild(pivoltanteX)
-  } else {
-    pivoltanteX.style.display = 'none'
+  if (MdOptions == 'pivoltante') {
+    imgDi.innerHTML = ""
+    mgp = document.createElement('img')
+    mgp.src = pi
+    imgDi.appendChild(mgp)
+    arr64.push(piX)
+
   }
 
-  if (MdOptions == 'deslisanteporta') {
-    deslisanteporta.style.display = 'block'
-    result05.appendChild(deslisanteporta)
-  } else {
-    deslisanteporta.style.display = 'none'
+  if (MdOptions == 'janela2B') {
+    imgDi.innerHTML = ""
+    mgj2 = document.createElement('img')
+    mgj2.src = ja2
+    imgDi.appendChild(mgj2)
+    arr64.push(jaX2)
+
   }
 
-  if (MdOptions == 'versatikporta') {
-    versatikporta.style.display = 'block'
-    result05.appendChild(versatikporta)
-  } else {
-    versatikporta.style.display = 'none'
+  if (MdOptions == 'janela4B') {
+    imgDi.innerHTML = ""
+    mgj4 = document.createElement('img')
+    mgj4.src = ja4
+    imgDi.appendChild(mgj4)
+    arr64.push(jaX4)
+
   }
+
+  if (MdOptions == 'portaP') {
+    imgDi.innerHTML = ""
+    mgpa = document.createElement('img')
+    mgpa.src = pa
+    imgDi.appendChild(mgpa)
+    arr64.push(paX)
+
+  }
+
+  if (MdOptions == 'portaP2B') {
+    imgDi.innerHTML = ""
+    mgpa2 = document.createElement('img')
+    mgpa2.src = pa2
+    imgDi.appendChild(mgpa2)
+    arr64.push(paX2)
+
+  }
+  if (MdOptions == 'porta2B') {
+    imgDi.innerHTML = ""
+    mgpa2 = document.createElement('img')
+    mgpa2.src = po2
+    imgDi.appendChild(mgpa2)
+    arr64.push(paX2)
+
+  }
+
+  if (MdOptions == 'porta4B') {
+    imgDi.innerHTML = ""
+    mgpo4 = document.createElement('img')
+    mgpo4.src = po4
+    imgDi.appendChild(mgpo4)
+    arr64.push(poX4)
+
+  }
+  if (MdOptions == 'portaCorrer') {
+    imgDi.innerHTML = ""
+    mgco = document.createElement('img')
+    mgco.src = co
+    imgDi.appendChild(mgco)
+    arr64.push(coX)
+
+  }
+  if (MdOptions == 'portaCorrer2B') {
+    imgDi.innerHTML = ""
+    mgco = document.createElement('img')
+    mgco.src = co2
+    imgDi.appendChild(mgco)
+    arr64.push(coX2)
+
+  }
+  if (MdOptions == 'portaJumbo') {
+    imgDi.innerHTML = ""
+    mgpj = document.createElement('img')
+    mgpj.src = pj
+    imgDi.appendChild(mgpj)
+
+  }
+  if (MdOptions == 'portaDeslisante') {
+    imgDi.innerHTML = ""
+    mgdl = document.createElement('img')
+    mgdl.src = dl
+    imgDi.appendChild(mgdl)
+    arr64.push(deX)
+  }
+
+  if (MdOptions == 'portaVersatik') {
+    imgDi.innerHTML = ""
+    mgvi = document.createElement('img')
+    mgvi.src = vs
+    imgDi.appendChild(mgvi)
+    arr64.push(veX)
+  }
+
+  return;
 
 }
+
+
+
+//porcetagem do orçamento 
 
 function porcentoSo() {
 
@@ -346,6 +387,8 @@ function porcentoSo() {
 
 }
 
+//calcular mao de obra
+
 function Px() {
   let inputN = xy('.entrarP').value;
   let porcentoX2 = xy('.porcento2');
@@ -353,16 +396,129 @@ function Px() {
   let smpT = parseInt(Options) * parseInt(inputN);
   let smpX = parseInt(smpT) / 100;
   let smpF = parseInt(smpX) + parseInt(inputN);
-
   let result12 = xy('.result12');
-
   result12.innerHTML = `Valor Final: R$: ${smpF}`
+}
 
+//Criar imagem dinamicamente
 
+function CriarImage() {
+  let contente4 = xy(".contente4")
+  for (let i = 0; i < arr.length; i++) {
+    img = document.createElement('img');
+    img.src = arr[i]
+    contente4.appendChild(img)
+  }
+}
+CriarImage()
+//Criar imagem dinamicamente para Via do cliente 
+
+function AddImage() {
+
+  let contentevia = xy(".contente21")
+
+  let arr2 = arr64.filter(function (e, i) {
+    return arr64.indexOf(e) === i
+  });
+
+  for (let i = 0; i < arr2.length; i++) {
+    img = arr2[i]
+    contentevia.appendChild(img)
+  }
+}
+
+//criando imagens das ferragens
+
+ferragens.map((el) => {
+  let imgF = xy('.clone').cloneNode(true);
+  xy('.cont3').append(imgF);
+  imgF.querySelector('.foto img').src = el.img;
+  imgF.querySelector('.fotonome').innerHTML = el.nome
+
+})
+
+//Gerar imagems base64
+SalvarPdf.addEventListener('click', () => {
+  //comparae elementos repetidos nos arrays
+  const arr64Verificado = arr64.filter(function (el, index) {
+    return arr64.indexOf(el) === index
+  })
+
+  for (let b = 0; b < arr64Verificado.length; b++) {
+    let img64 = arr64Verificado[b];
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = 700
+    canvas.height = 1700
+    ctx.drawImage(img64, 5, 620, 300, 300);
+    let imgbase64 = canvas.toDataURL();
+
+    imgconvert.push(imgbase64)
+
+  }
+
+  GerarPDF();
+});
+
+//Gerar arquivo em PDF
+
+function GerarPDF() {
+  let conteY = xy('.contente21')
+
+  var doc = new jsPDF()
+  for (let g = 0; g < imgconvert.length; g++) {
+    let pdfFinal = imgconvert[g]
+    doc.fromHTML(conteY, 10, 0)
+    doc.addImage(pdfFinal, 10, 1, 300, 500)
+
+  }
+  doc.save('Orcamento.pdf')
 
 }
 
 
+// funcao para imprimir documento
 
+function Imprimir() {
+  let ocultarMenu = xy('.listadiv')
+  let im = xy('.btn')
+  ocultarMenu.style.opacity = '0'
+  im.style.opacity = '0'
+  window.print()
+  ocultarMenu.style.opacity = '1'
+  im.style.opacity = '1'
+}
 
+// funcao para remderizar imagens
 
+function DimencaoImg() {
+  let img = xys('.contente21 img')
+  if (img.length == 1) {
+    img.forEach((el) => {
+      let img2 = el
+      img2.style.width = '550px'
+      img2.style.height = '550px'
+      img2.style.marginTop = "130px"
+      img2.style.marginLeft = "130px"
+    })
+  }
+}
+
+//salvar valor final para o cliente
+
+function ValorCliente() {
+  let moscli = xy('.contente21')
+  let clienteFinal = xy('.clienteFinal').value;
+  let clienteFinal2 = clienteFinal.replace(/\D+/g, '');
+  let objeto = new Array();
+  if (JSON.parse(localStorage.getItem("objeto")) != null) {
+    localarray.push(JSON.parse(localStorage.getItem("objeto")));
+  }
+  localarray.push([clienteFinal2]);
+  let pessoaJson = JSON.stringify(localarray);
+  localStorage.setItem("objeto", pessoaJson);
+  let resultFinal = parseInt(clienteFinal2) + parseInt(calcularSoma)
+
+  moscli.insertAdjacentHTML('beforeend', `<p id="somaBruta">${[`Valor Bruto R\$ ${resultFinal}`]}</p>`);
+
+}
